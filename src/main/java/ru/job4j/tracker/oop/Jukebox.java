c3a288cf68f5ea65fd48af5bbc6d8d5e36c41854
Zeoforex <dshoma853@gmail.com>
2021-11-01 20:29:46 +0300

    1.3. Вызов метода с аргументами. [#166086 #223348]

--- src/main/java/ru/job4j/tracker/oop/Jukebox.java
+++ src/main/java/ru/job4j/tracker/oop/Jukebox.java
@@ -10,6 +10,7 @@ public class Jukebox {
             System.out.println("Песня не найдена");
         }
     }
+
     public static void main(String[] args) {
         Jukebox turntable = new Jukebox();
         turntable.music(1);
