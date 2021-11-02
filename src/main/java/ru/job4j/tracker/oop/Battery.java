95cfb3c7e68c21bc8443d6520a9b102681a504d2
Zeoforex <dshoma853@gmail.com>
2021-11-02 17:32:59 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int percent) {
+        this.load = percent;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(15);
+        Battery powerBank = new Battery(2);
+        System.out.println("battery: " + battery.load + ". powerBank: " + powerBank.load);
+        battery.exchange(powerBank);
+        System.out.println("battery: " + battery.load + ". powerBank: " + powerBank.load);
+    }
+}
