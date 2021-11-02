ef1e1973a39c923a51a889c7c83cf1fe3e4b734b
Zeoforex <dshoma853@gmail.com>
2021-11-02 16:12:58 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,15 +1,33 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String name;
+    private String food;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println("Cat's nickname: " + this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.print("There are gav's food: ");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Гав");
+        gav.show();
+        System.out.print("There are black's food: ");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Черный кот");
+        black.show();
     }
 
 }
