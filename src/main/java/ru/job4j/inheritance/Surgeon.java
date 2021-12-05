1a241bf34e077c6846dda49a21ce171678c877ac
Zeoforex <dshoma853@gmail.com>
2021-12-05 19:35:46 +0300

    3. Реализация профессий в коде [#6837 #235421]

--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -3,8 +3,9 @@ package ru.job4j.inheritance;
 public class Surgeon extends Doctor {
     private int yearsOfOperation;
 
-    public Surgeon(String name, String surname, String education, int birthday, String hospital) {
+    public Surgeon(String name, String surname, String education, int birthday, String hospital, int yearsOfOperation) {
         super(name, surname, education, birthday, hospital);
+        this.yearsOfOperation = yearsOfOperation;
     }
 
     public void operation(Patient patient) {
