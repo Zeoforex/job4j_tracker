fc7ab3fb1abca1848c8298879f6386ba927dd835
Zeoforex <dshoma853@gmail.com>
2022-03-06 12:06:29 +0300

    5. Tracker - хранилище [#396 #272003]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -34,7 +34,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = new Item[items.length];
+        Item[] rsl = new Item[size];
         int count = 0;
         for (int i = 0; i < size; i++) {
             if (key.equals(items[i].getName())) {
