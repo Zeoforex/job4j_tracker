ab8d126ea921b6079aa9883bee962495fdcd736c
Zeoforex <dshoma853@gmail.com>
2022-03-14 16:37:48 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #274430]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,12 +2,10 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
-    /* Добавим в tracker новую заявку */
-
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
@@ -22,38 +20,4 @@ public class StartUITest {
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
-        Item item = tracker.add(new Item("Replaced item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
-        String replacedName = "New item name";
-        Input in = new StubInput(
-                new String[] {"0" /* входные параметры для ReplaceAction */, "1"}
-        );
-        UserAction[] actions = {
-                new ReplaceAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        /* Добавим в tracker новую заявку */
-        Item item = tracker.add(new Item("Deleted item"));
-        /* Входные данные должны содержать ID добавленной заявки item.getId() */
-        Input in = new StubInput(
-                new String[] {"0" /* входные параметры для DeleteAction */, "1"}
-        );
-        UserAction[] actions = {
-                new DeleteAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
-    }
 }
