17357024d7695bb78281136cce100649856214c7
Zeoforex <dshoma853@gmail.com>
2022-03-15 14:41:37 +0300

    Вернусь к последнему

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,96 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    @Test
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input in = new StubInput(new String[]{"0", String.valueOf(one.getId()), replaceName, "1"});
+        UserAction[] actions = new UserAction[]{
+                new ReplaceAction(out), new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("Menu:" + ln + "0.Edit item" + ln + "1.Exit Program"
+                + ln + "=== Edit item ===" + ln + "Заявка изменена успешно." + ln + "Menu:"
+                + ln + "0.Edit item"
+                + ln + "1.Exit Program" + ln + "=== Exit Program ===" + ln));
+    }
+
+    @Test
+    public void whenFindAllItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(new String[]{"0", "1"});
+        UserAction[] actions = new UserAction[]{
+                new FindAllAction(out), new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("Menu:" + ln + "0.Show all items"
+                + ln + "1.Exit Program" + ln + "=== Show all items ==="
+                + ln + one + ln + "Menu:"
+                + ln + "0.Show all items" + ln + "1.Exit Program"
+                + ln + "=== Exit Program ===" + ln));
+    }
+
+    @Test
+    public void whenFindByNameItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String findName = "test1";
+        Input in = new StubInput(new String[]{"0", findName, "1"});
+        UserAction[] actions = new UserAction[]{new FindByNameAction(out), new Exit(out)};
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("Menu:" + ln
+                + "0.Find items by name" + ln
+                + "1.Exit Program" + ln
+                + "=== Find items by name ===" + ln
+                + one + ln
+                + "Menu:" + ln
+                + "0.Find items by name" + ln
+                + "1.Exit Program" + ln
+                + "=== Exit Program ===" + ln
+        ));
+    }
+
+    @Test
+    public void whenFindItemByIdTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("id"));
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(one.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByIdAction(out),
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0.Find item by id" + ln
+                        + "1.Exit Program" + ln
+                        + "=== Find item by id ===" + ln
+                        + one + ln
+                        + "Menu:" + ln
+                        + "0.Find item by id" + ln
+                        + "1.Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+        ));
+    }
+
     @Test
     public void whenExit() {
         Output out = new StubOutput();
