ec55aa561be7e6fa156c8e6ba880c9cd69665b3b
Zeoforex <dshoma853@gmail.com>
2022-03-15 14:46:51 +0300

    0.1. java.lang.NullPointerException [#219364 #275138]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
-import org.junit.Test;
 
+import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsNull.nullValue;
