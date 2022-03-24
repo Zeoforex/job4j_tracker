6ab8309bc176cff61afa8cdb5bf9f685387a3089
Zeoforex <dshoma853@gmail.com>
2022-03-24 18:30:04 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultipleValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"1", "2", "4"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        ValidateInput input1 = new ValidateInput(out, in);
+        int selected1 = input1.askInt("Enter menu:");
+        assertThat(selected1, is(2));
+        ValidateInput input2 = new ValidateInput(out, in);
+        int selected2 = input2.askInt("Enter menu:");
+        assertThat(selected2, is(4));
+    }
+
+    @Test
+    public void whenInvalidMinusInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
+}
