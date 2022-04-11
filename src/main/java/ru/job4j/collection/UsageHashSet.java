c4d7049e48abb14327f0521109f5f69623f13022
Zeoforex <dshoma853@gmail.com>
2022-04-11 17:28:30 +0300

    0. Set, HashSet, Iterator. [#10095 #284867]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -9,6 +9,10 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
+        autos.add("Volvo");
+        autos.add("BMW");
+        autos.add("Lada");
+        autos.add("Lada");
         for (String name : autos) {
             System.out.println(name);
         }
