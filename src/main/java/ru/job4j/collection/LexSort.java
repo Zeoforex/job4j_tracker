19f906281996bb591fd1ed3d8f9ff59cba4a40e1
Zeoforex <dshoma853@gmail.com>
2022-04-20 12:18:23 +0300

    4. Сортировка номера [#221284 #291072]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -1,16 +1,10 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] splitLeft = left.split("[^0-9]");
-        String[] splitRight = right.split("[^0-9]");
-        String sLeft = String.join(" ", splitLeft);
-        String sRight = String.join(" ", splitRight);
-        int leftCompare = Integer.parseInt(sLeft);
-        int rightCompare = Integer.parseInt(sRight);
-        return Integer.compare(leftCompare, rightCompare);
+        return Integer.compare(Integer.parseInt(String.valueOf(left.split("\\.")[0])),
+                Integer.parseInt(String.valueOf(right.split("\\.")[0])));
     }
 }
